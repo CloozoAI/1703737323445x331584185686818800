af9c43ef1b8cb274054e058b6327f48a4ac6edf6
Simarpreet Singh <ssimarpreet11@gmail.com>
2024-01-22 16:24:11 +0530

    message

---
+++ Samyra_Core.js
@@ -0,0 +1,1 @@
+import Chatbot from "https://cdn.jsdelivr.net/gh/Capson/widget@v116/dist/web.js"     Chatbot.init({         chatflowid: "1b7ad6e5-8361-4020-86a8-a2e9cbe4a7df",         apiHost: "https://codecloozo-production.up.railway.app",         chatflowConfig: {           predefinedQuestions: [],           qdrantCollection:””         }                    })
