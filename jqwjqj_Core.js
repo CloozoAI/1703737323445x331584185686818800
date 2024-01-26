92024cdc73f0c5529433a82087494de3c4fede54
Simarpreet Singh <ssimarpreet11@gmail.com>
2024-01-26 11:16:07 +0530

    message

---
+++ jqwjqj_Core.js
@@ -0,0 +1,44 @@
+import Chatbot from "https://cdn.jsdelivr.net/gh/Capson/widget@v116/dist/web.js"
+    Chatbot.init({
+        chatflowid: "efde6ef2-c3fe-45ef-b406-ddebea1b9a09",
+        apiHost: "https://codecloozo-production.up.railway.app",
+        chatflowConfig: {
+          predefinedQuestions: []
+        },
+ theme: {
+            button: {
+                backgroundColor: "#1a1a1a",
+                right: 20,
+                bottom: 20,
+                size: "large",
+                iconColor: "white",
+                customIconSrc: "https://img.icons8.com/serif/64/FFFFFF/experimental-chat-serif.png",
+            },
+ chatWindow: {
+                welcomeMessage: "Hello! I am a Google bot. How can I assist you?",
+                backgroundColor: "#ffffff",
+                height: 700,
+                width: 400,
+                fontSize: 16,
+                poweredByTextColor: "#303235",
+                botMessage: {
+                    backgroundColor: "#f7f8ff",
+                    textColor: "#303235",
+                    showAvatar: false,
+                    avatarSrc: "https://raw.githubusercontent.com/zahidkhawaja/langchain-chat-nextjs/main/public/parroticon.png",
+                },
+                userMessage: {
+                    backgroundColor: "#3B81F6",
+                    textColor: "#ffffff",
+                    showAvatar: false,
+                    avatarSrc: "https://raw.githubusercontent.com/zahidkhawaja/langchain-chat-nextjs/main/public/usericon.png",
+                },
+                textInput: {
+                    placeholder: "Type your question",
+                    backgroundColor: "#ffffff",
+                    textColor: "#303235",
+                    sendButtonColor: "#3B81F6",
+                }
+            }
+        }
+    })
